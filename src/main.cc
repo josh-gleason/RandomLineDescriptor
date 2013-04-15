13b5206aafb9de507c15432c2d0cd917d805493f
Joshua Gleason <gleaso22@gmail.com>
2013-04-15 01:14:54 -0700

    computes features for image

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,14 @@ typedef double FType;
 
 struct ProgramSettings {
    struct MserSettings {
+      MserSettings() :
+         delta(2),
+         minArea(0.00015),
+         maxArea(0.35),
+         maxVariation(0.25),
+         minDiversity(0.2)
+      {}
+
       int delta;
       double minArea;
       double maxArea;
@@ -19,17 +27,60 @@ struct ProgramSettings {
       double minDiversity;
    };
    
+   struct DescriptorSettings {
+      DescriptorSettings() :
+         ellipseSize(2.0),
+         ellipsePoints(512U),
+         l(32),
+         Nk(150),
+         N(150),
+         minDist(0.25),
+         k1(1.0),
+         p1(1.0),
+         p2(1.0),
+         w1(1.0),
+         w2(1.0)
+      {}
+
+      double ellipseSize;  // scale factor of ellipse to min bounding box
+      size_t ellipsePoints;   // number of points to sample around perimeter of ellipse
+
+      int l;   // number of points per line
+      int Nk;  // number of lines per region in reference region
+      int N;   // number of lines per region in test region
+
+      double minDist; // minimum allowed length of line (with respect to sqrt(area) of region)
+
+      double k1;  // Dmax weighting (0,1]
+      double p1;  // controls shape of confidence function
+      double p2;  // controls shape of confidence function
+      double w1;  // weight for distance
+      double w2;  // weight for error
+   };
+
    MserSettings mser;
+   
+   DescriptorSettings descriptor;
+};
+
+struct Region {
+   RotatedRect ellipse;
+   vector<Line> lines;
+   vector<Mat> descriptors;
+   vector<double> mean;
+   vector<double> err;
+
+   double errMax;
 };
 
 void getEllipsePoints(vector<Point2d>& vertices, const RotatedRect& box, size_t points=1000U);
 void drawEllipse(Mat& image, const RotatedRect& box, const Scalar& color = Scalar(0,0,255));
 void drawEllipse(Mat& image, const vector<Point2d>& vertices, const Scalar& color = Scalar(0,0,255));
-void genLines(vector<Line>& pairs, const vector<Point2d>& vertices, double minDist=1.0, size_t pairCount=50);
+void genLines(Region& region, const vector<Point2d>& vertices, double minDist=1.0, size_t pairCount=50);
 
-void genLines(vector<Line>& lines, const vector<Point2d>& vertices, double minDist, size_t pairCount)
+void genLines(Region& region, const vector<Point2d>& vertices, double minDist, size_t pairCount)
 {
-   lines.resize(pairCount);
+   region.lines.resize(pairCount);
 
    for ( int i = 0; i < pairCount; ++i )
    {
@@ -50,7 +101,7 @@ void genLines(vector<Line>& lines, const vector<Point2d>& vertices, double minDi
          return;
       }
 
-      lines[i] = p;
+      region.lines[i] = p;
    }
 }
 
@@ -125,33 +176,38 @@ void drawLines(Mat& image, const vector<Line>& lines, const Scalar& color)
 
 double distFunc(const Mat& first, const Mat& second)
 {
+   // difference
    Mat diff = first - second;
-
-   // square
+   
+   // squared
    diff = diff.mul(diff);
-
-   // set last element (holds the mean before normalization) to zero
-   diff.at<FType>(diff.total()-1) = 0;
   
-   // mean is pre-subtracted
+   // mean is pre-subtracted so no -delta function
    return sum(diff)[0];
 }
 
-void buildFeatures(vector<Mat>& features, const Mat& image, vector<Line>& lines, int featureSize)
+// image must be grayscale
+void buildFeatures(Region& region, const Mat& image, int featureSize)
 {
-   features.clear();
-   features.resize(lines.size());
+   size_t descriptorCount = region.lines.size();
+
+   region.descriptors.resize(descriptorCount);
+   region.mean.resize(descriptorCount);
+   region.err.resize(descriptorCount);
 
    // features
-   // pixel1, pixel2, pixel3 ..., pixelN, mean
+   // pixel1, pixel2, pixel3 ..., pixelN, mean, err
 
-   for ( size_t i = 0; i < lines.size(); ++i )
+   // mean = sum(pixels)/(featureSize-2)
+   // err = sum(|pixel_n - pixel_n+1|)_n={1,featureSize-3}/(featureSize-3)
+
+   for ( size_t i = 0; i < descriptorCount; ++i )
    {
-      Mat& feat = features[i];
+      Mat& feat = region.descriptors[i];
       feat.create(1, featureSize, CV_FTYPE);
 
-      Point2d& p1 = lines[i].first;
-      Point2d& p2 = lines[i].second;
+      Point2d& p1 = region.lines[i].first;
+      Point2d& p2 = region.lines[i].second;
 
       Point2d sample = p1;
 
@@ -159,11 +215,15 @@ void buildFeatures(vector<Mat>& features, const Mat& image, vector<Line>& lines,
                    (p2.y - p1.y) / (featureSize - 1.0));
 
       double sum = 0.0;
+      double diffSum = 0.0;
       
-      for ( int j = 0; j < featureSize-1; ++j )
+      for ( int j = 0; j < featureSize; ++j )
       {
          feat.at<FType>(j) = image.at<unsigned char>(Point2i(sample.x,sample.y));
          sum += feat.at<FType>(j);
+
+         if ( j != 0 )
+            diffSum += fabs(feat.at<FType>(j-1)-feat.at<FType>(j));
 
          // increment step
          sample.x += step.x;
@@ -171,14 +231,14 @@ void buildFeatures(vector<Mat>& features, const Mat& image, vector<Line>& lines,
       }
 
       // store mean
-      FType mean = (FType)(sum / (featureSize - 1.0));
-      
+      region.mean[i] = (FType)(sum / (featureSize));
+      region.err[i] = (FType)(diffSum / (featureSize - 1.0));
+
       // subtract mean for feat
-      feat -= mean;
-     
-      // save the old mean so we can get original value back
-      feat.at<FType>(featureSize-1) = mean;
+      feat -= region.mean[i];
    }
+
+   region.errMax = *max_element(region.err.begin(), region.err.end());
 }
 
 int main(int argc, char *argv[])
@@ -188,66 +248,42 @@ int main(int argc, char *argv[])
    // Reject threshold based on how many matches belonged to first vs. second class
    //    Reject threhold for precision recall?
 
+   srand(time(0));
+#if 0
+   ProgramSettings settings;
+   
+   settings.descriptor.l = 20;
+   settings.descriptor.N = 50;
+
    // image of black
    Mat img = imread(argv[1]);
    Mat grayImg;
    cvtColor(img, grayImg, CV_RGB2GRAY);
 
-   RotatedRect r(Point2f(250.0f, 250.0f), Size2f(400.0f,300.0f), 100);
-
-   double threshold = min(r.size.width, r.size.height)/2.0;
+   RotatedRect r(Point2f(450.0f, 250.0f), Size2f(400.0f,300.0f), 100);
 
    vector<Point2d> vertices(1000U);
-   vector<Line> lines;
-   vector<Mat> features;
-   
-   double featureSize = 21; // 20 samples and 1 mean stored
+   Region region;
 
-   getEllipsePoints(vertices, r, 1000U);
-   genLines(lines, vertices, threshold, 50);
+   region.ellipse = RotatedRect(Point2f(450.0f, 250.0f), Size2f(400.0f,300.0f), 100);
+
+   // get 1000 points on the ellipse
+   getEllipsePoints(vertices, r, 512U);
+   
+   double minDist = settings.descriptor.minDist * sqrt(r.size.area());
+   genLines(region, vertices, minDist, 50);
 
    drawEllipse(img, vertices, Scalar(255,0,0));
-   drawLines(img, lines, Scalar(255,255,255));
+   drawLines(img, region.lines, Scalar(255,255,255));
    
-   buildFeatures(features, grayImg, lines, featureSize);
-
-
-   Mat a(1,6,CV_FTYPE);
-   Mat b(1,6,CV_FTYPE);
-   
-   a.at<FType>(0) = -20;
-   a.at<FType>(1) = -10;
-   a.at<FType>(2) = 0;
-   a.at<FType>(3) = 10;
-   a.at<FType>(4) = 20;
-   a.at<FType>(5) = 20;
-
-   b.at<FType>(0) = -40;
-   b.at<FType>(1) = -20;
-   b.at<FType>(2) = 0;
-   b.at<FType>(3) = 20;
-   b.at<FType>(4) = 40;
-   b.at<FType>(5) = 40;
-   
-   Mat c = a.clone();
-   Mat d = b.clone();
-
-   c += 20;
-   d += 40;
-
-   c.at<FType>(5) = 20;
-   d.at<FType>(5) = 40;
-
-   cout << distFunc(a, b) << endl;
-   cout << distFunc(c, d) << endl;
-   cout << distFunc(features[1], features[0]) << endl;
+   buildFeatures(region, grayImg, settings.descriptor.l);
 
    imshow("Image", img);
    waitKey(0);
 
    return 0;
-
-#if 0
+#else
+   
    if ( argc < 3 ) {
       cout << "Usage: " << argv[0] << " <input image> <output image>" << endl;
       return -1;
@@ -299,12 +335,38 @@ int main(int argc, char *argv[])
 
    mserDetector(grayImage, keypoints);
 
+   size_t regionCount = 0;
+   vector<RotatedRect> ellipses;
    for ( const vector<Point>& contour : keypoints ) {
       RotatedRect box = minAreaRect(contour);
-//      ellipse( image, box, Scalar(255, 0, 0), 2 );
-      drawEllipse( image, box, Scalar(255, 0, 0) );
+      box.size.width *= settings.descriptor.ellipseSize;
+      box.size.height *= settings.descriptor.ellipseSize;
+      // TODO reject very high eccentricity regions
+      if ( (Rect(Point(0,0), image.size()) & box.boundingRect()).size() ==
+            box.boundingRect().size() )
+      {
+         drawEllipse( image, box, Scalar(255, 0, 0) );
+         ellipses.push_back(box);
+      }
    }
 
+   ///////// Build descriptors
+   // initialize region list
+   vector<Region> regions(ellipses.size());
+   
+   vector<Point2d> vertices;
+   for ( int i = 0; i < ellipses.size(); ++i )
+   {
+      RotatedRect& r = ellipses[i];
+      regions[i].ellipse = r;
+      getEllipsePoints(vertices, r, settings.descriptor.ellipsePoints);
+
+      double minDist = settings.descriptor.minDist * sqrt(r.size.area());
+      genLines(regions[i], vertices, minDist, settings.descriptor.Nk);
+
+      buildFeatures(regions[i], grayImage, settings.descriptor.l);
+   }
+   
    imwrite(argv[2], image);
 
    waitKey(0);
@@ -312,3 +374,4 @@ int main(int argc, char *argv[])
    return 0;
 #endif
 }
+
